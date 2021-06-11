983b984917ec41d5206f3f5d2379ad4471adb4b1
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-11 14:13:29 +0000

    update

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Installing dependencies 
-sudo apt-get install python3-venv
-sudo apt-get install -y python3.6-venv python3-pip -y
-sudo pip3 install virtualenv
+sudo apt update
+sudo apt-get install -y python3-venv python3-pip -y
 # Creating and Activating virtual environment "venv" and installing requirements
 python3 -m venv venv
 source venv/bin/activate
