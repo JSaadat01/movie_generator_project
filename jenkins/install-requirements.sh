a7cb8311ccdb8a600e6ed7518e1e96e7907ebfd5
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-12 22:22:29 +0000

    update

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt update
 sudo apt install -y curl jq
 
@@ -12,4 +11,7 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # Docker login
-docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD 
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+
+echo Username: $DOCKER_USERNAME
+echo Password: $DOCKER_PASSWORD
