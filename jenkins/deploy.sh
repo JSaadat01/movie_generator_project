ae50b96f9d17e577d01979a1fd5c885b15fe6eba
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-13 22:42:56 +0000

    ok

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-scp -i ~/.ssh
-ssh -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager
-docker stack deploy --compose-file docker-compose.yaml movie_generator_project
+# copy over compose yaml to manager node
+ssh -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+
+# docker stack deploy
+scp -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+    export DATABASE_URI=$(DATABASE_URI)
+    docker stack deploy --compose-file docker-compose.yaml movie_generator_project
+EOF
