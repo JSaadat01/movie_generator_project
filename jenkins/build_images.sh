2d8bf8ffae40bd7bb41671b22a497a44c02e6791
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-14 00:48:42 +0000

    changes

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Build and push images
-sudo docker-compose build
+docker-compose down --rmi all
+docker-compose build
+docker-compose up -d
 sudo docker-compose push
+export DATABASE_URI=mysql+pymysql://root:root@35.197.207.20:3306/project
+docker exec movie-gen_frontend_service python /app/create.py
