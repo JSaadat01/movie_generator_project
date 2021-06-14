4b03f8041161c086cff1b051b2868819da163904
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-14 09:36:37 +0000

    new

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -4,8 +4,8 @@ from flask import request, Response
 @app.route('/movie', methods=["POST"])
 def movie():
     movies = {
-    "London 24" : {"Sunny 24" : "Avengers: Infinity War 48", "Rainy 24" : "Schindler's List 48", "Windy 24" : "The Shawshank Redemption 48"}
-    "New Delhi 24" : {"Sunny 24" : "Khaali Peeli 48", "Rainy 24" : "Bulbbul 48", "Windy 24" : "Devdas 48"}
+    "London 24" : {"Sunny 24" : "Avengers: Infinity War 48", "Rainy 24" : "Schindler's List 48", "Windy 24" : "The Shawshank Redemption 48"},
+    "New Delhi 24" : {"Sunny 24" : "Khaali Peeli 48", "Rainy 24" : "Bulbbul 48", "Windy 24" : "Devdas 48"},
     "Tokyo 24" : {"Sunny 24" : "Rurouni Kenshin 48", "Rainy 24" : "Ring 48", "Windy 24" : "Midnight Sun 48"}
     }
     info = request.json 
