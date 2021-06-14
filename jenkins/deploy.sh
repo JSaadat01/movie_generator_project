1ce687ebd3f6ee7996502fca81e401403c00c499
JSaadat01 <JSaadat@development.europe-west2-c.c.skilful-bliss-311909.internal>
2021-06-14 11:28:32 +0000

    changes

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,5 @@
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     export DATABASE_URI=$(DATABASE_URI)
-    docker stack deploy --compose-file docker-compose.yaml movie-gen
+    docker stack deploy --compose-file docker-compose.yaml movie_generator_project
 EOF
